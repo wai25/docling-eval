9448f9a6e5af86903ec6052caa594026c2268647
Nikos Livathinos <100353117+nikos-livathinos@users.noreply.github.com>
2025-01-23 18:36:52 +0100

    fix: Replace NLTK with HuggingFace evaluate library to compute the BLEU score (#14)
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

--- docling_eval/evaluators/markdown_text_evaluator.py
+++ docling_eval/evaluators/markdown_text_evaluator.py
@@ -2,17 +2,15 @@ import logging
 from pathlib import Path
 from typing import Dict, List, Tuple
 
-import nltk
+import evaluate
 from datasets import load_dataset
 from docling_core.types.doc.base import ImageRefMode
 from docling_core.types.doc.document import DoclingDocument
-from nltk.tokenize import word_tokenize
 from pydantic import BaseModel
 from tqdm import tqdm  # type: ignore
 
 from docling_eval.benchmarks.constants import BenchMarkColumns  # type: ignore
 from docling_eval.evaluators.stats import DatasetStatistics, compute_stats
-from docling_eval.utils.bleu import compute_bleu_score
 
 _log = logging.getLogger(__name__)
 
@@ -32,8 +30,7 @@ class DatasetMarkdownEvaluation(BaseModel):
 
 class MarkdownTextEvaluator:
     def __init__(self):
-        # Download the necessary NLTK data for the tokenizer
-        nltk.download("punkt_tab", quiet=True)
+        self._bleu_eval = evaluate.load("bleu")
 
     def __call__(self, ds_path: Path, split: str = "test") -> DatasetMarkdownEvaluation:
         parquet_files = str(ds_path / split / "*.parquet")
@@ -45,7 +42,6 @@ class MarkdownTextEvaluator:
         evaluations: list[PageMarkdownEvaluation] = []
         bleus = []
 
-        broken_inputs = 0
         for i, data in tqdm(
             enumerate(ds_selection),
             desc="Markdown text evaluations",
@@ -64,11 +60,11 @@ class MarkdownTextEvaluator:
 
             # Export to markdown and tokenize
             true_md = true_doc.export_to_markdown(image_mode=ImageRefMode.PLACEHOLDER)
-            true_tokens = word_tokenize(true_md)
             pred_md = pred_doc.export_to_markdown(image_mode=ImageRefMode.PLACEHOLDER)
-            pred_tokens = word_tokenize(pred_md)
+            bleu = 0.0
+            if true_md != "" and pred_md != "":
+                bleu = self._compute_bleu_score(true_md, pred_md)
 
-            bleu = compute_bleu_score(true_tokens, pred_tokens)
             bleus.append(bleu)
             md_evaluation = PageMarkdownEvaluation(
                 doc_id=doc_id, true_md=true_md, pred_md=pred_md, bleu=bleu
@@ -79,3 +75,13 @@ class MarkdownTextEvaluator:
             evaluations=evaluations, bleu_stats=bleu_stats
         )
         return ds_md_evalutions
+
+    def _compute_bleu_score(self, true_txt: str, pred_txt: str) -> float:
+        r"""
+        Compute BLEU score with the HF evaluate and the default Tokenizer_13
+        """
+        result = self._bleu_eval.compute(
+            predictions=[pred_txt], references=[[true_txt]]
+        )
+        bleu = result["bleu"]
+        return bleu
--- docling_eval/utils/bleu.py
+++ docling_eval/utils/bleu.py
@@ -1,53 +1,53 @@
-from nltk.translate.bleu_score import corpus_bleu
+# from nltk.translate.bleu_score import corpus_bleu
 
 
-def compute_bleu_scores(
-    targets: list[list[str]], predictions: list[list[str]]
-) -> tuple[list[float], float]:
-    r"""
-    Compute the BLEU score for the given targets and predictions.
+# def compute_bleu_scores(
+#     targets: list[list[str]], predictions: list[list[str]]
+# ) -> tuple[list[float], float]:
+#     r"""
+#     Compute the BLEU score for the given targets and predictions.
 
-    Parameters
-    ----------
-    targets : List[List[str]]
-        The ground truth target sequences.
-    predictions : List[List[str]]
-        The predicted sequences. Each prediction is a list of tokens.
+#     Parameters
+#     ----------
+#     targets : List[List[str]]
+#         The ground truth target sequences.
+#     predictions : List[List[str]]
+#         The predicted sequences. Each prediction is a list of tokens.
 
-    Returns
-    -------
-    Tuple[List[float], float]
-        A tuple containing:
-        - bleu_scores (List[float]): The BLEU scores of the predictions.
-        - avg_bleu_score (float): The average BLEU score across all predictions.
-    """
-    weights = (0.25, 0.25, 0.25, 0.25)
+#     Returns
+#     -------
+#     Tuple[List[float], float]
+#         A tuple containing:
+#         - bleu_scores (List[float]): The BLEU scores of the predictions.
+#         - avg_bleu_score (float): The average BLEU score across all predictions.
+#     """
+#     weights = (0.25, 0.25, 0.25, 0.25)
 
-    bleu_scores = [
-        corpus_bleu([[tg]], [pred], weights=weights)
-        for tg, pred in zip(targets, predictions)
-    ]
-    return bleu_scores, sum(bleu_scores) / len(bleu_scores)
+#     bleu_scores = [
+#         corpus_bleu([[tg]], [pred], weights=weights)
+#         for tg, pred in zip(targets, predictions)
+#     ]
+#     return bleu_scores, sum(bleu_scores) / len(bleu_scores)
 
 
-def compute_bleu_score(target: list[str], prediction: list[str]) -> float:
-    r"""
-    Compute the BLEU score for the given targetand prediction text.
+# def compute_bleu_score(target: list[str], prediction: list[str]) -> float:
+#     r"""
+#     Compute the BLEU score for the given targetand prediction text.
 
-    Parameters
-    ----------
-    targets : List[List[str]]
-        The ground truth target sequences.
-    predictions : List[List[str]]
-        The predicted sequences. Each prediction is a list of tokens.
+#     Parameters
+#     ----------
+#     targets : List[List[str]]
+#         The ground truth target sequences.
+#     predictions : List[List[str]]
+#         The predicted sequences. Each prediction is a list of tokens.
 
-    Returns
-    -------
-    bleu_score
-    """
-    weights = (0.25, 0.25, 0.25, 0.25)
+#     Returns
+#     -------
+#     bleu_score
+#     """
+#     weights = (0.25, 0.25, 0.25, 0.25)
 
-    # reference: Ground truth (in BLEU there can be many references)
-    # hypothesis: prediction
-    bleu = corpus_bleu([[target]], [prediction], weights=weights)
-    return bleu
+#     # reference: Ground truth (in BLEU there can be many references)
+#     # hypothesis: prediction
+#     bleu = corpus_bleu([[target]], [prediction], weights=weights)
+#     return bleu
